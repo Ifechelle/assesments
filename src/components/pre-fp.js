a3bcfa03478366498c7cd5d172961bec4208ccad
Ifelola <tola.isesanya@gmail.com>
2022-04-14 18:08:43 -0400

    new

--- src/components/pre-fp.js
+++ src/components/pre-fp.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function PostPass() {
-    return(
+    const navigate = useNavigate()
+    const ForgotPassHandler = () => {
+        navigate("/forgotpassword")
+    }
+    const LoginHandler = () => {
+        navigate("/login")
+    }
+
+    return (
         <div>
-            The link has been sent to you email. If you can not find it click the link and try again. 
-            <in/>
+            The link has been sent to you email. If you can not find it click the link and try again.
+            <div onClick={ForgotPassHandler}>Forgot Password</div>
             If you have reset you password please log in again.
+            <div onClick={LoginHandler}> > </div>
         </div>
     )
 }
